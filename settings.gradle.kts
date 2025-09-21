0245a8c2681a7f09480167289d3423774ec17253
Raymie Stata <raymie.stata@airbnb.com>
2025-09-21 13:59:26 -0400

    Add dependency substitutions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,8 +15,37 @@ plugins {
 
 rootProject.name = "viaduct"
 
-includeBuild(".")
-includeBuild("included-builds/core")
+includeBuild(".") // TODO - is this needed?
+includeBuild("included-builds/core") {
+    dependencySubstitution {
+        // Engine modules
+        substitute(module("com.airbnb.viaduct:engine-api")).using(project(":engine:engine-api"))
+        substitute(module("com.airbnb.viaduct:engine-runtime")).using(project(":engine:engine-runtime"))
+
+        // Service modules
+        substitute(module("com.airbnb.viaduct:service-api")).using(project(":service:service-api"))
+        substitute(module("com.airbnb.viaduct:service-runtime")).using(project(":service:service-runtime"))
+        substitute(module("com.airbnb.viaduct:service-wiring")).using(project(":service:service-wiring"))
+
+        // Tenant modules
+        substitute(module("com.airbnb.viaduct:tenant-api")).using(project(":tenant:tenant-api"))
+        substitute(module("com.airbnb.viaduct:tenant-runtime")).using(project(":tenant:tenant-runtime"))
+
+        // Shared modules
+        substitute(module("com.airbnb.viaduct:shared-arbitrary")).using(project(":shared:shared-arbitrary"))
+        substitute(module("com.airbnb.viaduct:shared-dataloader")).using(project(":shared:shared-dataloader"))
+        substitute(module("com.airbnb.viaduct:shared-utils")).using(project(":shared:shared-utils"))
+        substitute(module("com.airbnb.viaduct:shared-logging")).using(project(":shared:shared-logging"))
+        substitute(module("com.airbnb.viaduct:shared-deferred")).using(project(":shared:shared-deferred"))
+        substitute(module("com.airbnb.viaduct:shared-graphql")).using(project(":shared:shared-graphql"))
+        substitute(module("com.airbnb.viaduct:shared-viaductschema")).using(project(":shared:shared-viaductschema"))
+        substitute(module("com.airbnb.viaduct:shared-invariants")).using(project(":shared:shared-invariants"))
+        substitute(module("com.airbnb.viaduct:shared-codegen")).using(project(":shared:shared-codegen"))
+
+        // Snipped modules
+        substitute(module("com.airbnb.viaduct:snipped-errors")).using(project(":snipped:snipped-errors"))
+    }
+}
 includeBuild("included-builds/codegen")
 includeBuild("gradle-plugins") {
     dependencySubstitution {
