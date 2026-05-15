f749a57640132733dd5d182e47d91b87cdcf07d8
Alex Ubillus <alexanderuv@users.noreply.github.com>
2026-05-15 17:09:00 +0000

    feat(testing): add ctx.rootFieldRef mocking for resolver tests (AIRBNB)
    Adds a way for tenant tests to stub the values returned by ctx.rootFieldRef
    and assert on the calls the resolver made.
    
    A recorder helper stubs return values per field, captures every call, and
    throws if a field is called more times than stubs were configured.
    
    Github-Change-Id: 1050229
    GitOrigin-RevId: 649854f19ab7d9803ce0b7326d5c9adda116bcf8

--- core/tenant/api/src/test/kotlin/viaduct/api/mocks/MocksTest.kt
+++ core/tenant/api/src/test/kotlin/viaduct/api/mocks/MocksTest.kt
@@ -1,4 +1,5 @@
 @file:Suppress("ForbiddenImport")
+@file:OptIn(viaduct.apiannotations.ExperimentalApi::class)
 
 package viaduct.api.mocks
 
@@ -11,8 +12,11 @@ import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import viaduct.api.globalid.GlobalID
+import viaduct.api.internal.RootObjectFieldImpl
 import viaduct.api.internal.internal
 import viaduct.api.reflect.Type
+import viaduct.api.types.Arguments
+import viaduct.api.types.GRT
 import viaduct.api.types.Object
 import viaduct.api.types.Query
 import viaduct.arbitrary.graphql.graphQLName
@@ -76,4 +80,149 @@ class MocksTest {
             loader.reflectionFor("Unknown")
         }
     }
+
+    private class FooParent : GRT
+
+    private class FooResult : Object
+
+    private val fooParent = Type.ofClass(FooParent::class)
+    private val fooResult = Type.ofClass(FooResult::class)
+
+    private fun fixedResult(value: Object) =
+        object : PrebakedRootFieldRefResults {
+            @Suppress("UNCHECKED_CAST")
+            override fun <A : Arguments, T : Object> get(
+                field: viaduct.api.reflect.RootObjectField<*, T, A>,
+                arguments: A
+            ): T = value as T
+        }
+
+    @Test
+    fun `rootFieldRef throws when no results are configured`() {
+        val ctx = MockResolverExecutionContext.create()
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        assertThrows<UnsupportedOperationException> {
+            ctx.rootFieldRef(field, Arguments.NoArguments)
+        }
+    }
+
+    @Test
+    fun `rootFieldRef returns the configured result`() {
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        val stub = FooResult()
+        val ctx = MockResolverExecutionContext<Query>(
+            internalContext = MockInternalContext(MockSchema.minimal),
+            rootFieldRefResults = fixedResult(stub),
+        )
+
+        assertSame(stub, ctx.rootFieldRef(field, Arguments.NoArguments))
+    }
+
+    @Test
+    fun `rootFieldRef forwards arguments to the results impl`() {
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        var capturedArgs: Arguments? = null
+        val capturing = object : PrebakedRootFieldRefResults {
+            @Suppress("UNCHECKED_CAST")
+            override fun <A : Arguments, T : Object> get(
+                field: viaduct.api.reflect.RootObjectField<*, T, A>,
+                arguments: A
+            ): T {
+                capturedArgs = arguments
+                return FooResult() as T
+            }
+        }
+        val ctx = MockResolverExecutionContext<Query>(
+            internalContext = MockInternalContext(MockSchema.minimal),
+            rootFieldRefResults = capturing,
+        )
+
+        ctx.rootFieldRef(field, Arguments.NoArguments)
+
+        assertSame(Arguments.NoArguments, capturedArgs)
+    }
+
+    @Test
+    fun `RecordingRootFieldRefResults consumes stubs in declaration order for the same field`() {
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        val first = FooResult()
+        val second = FooResult()
+        val recorder = RecordingRootFieldRefResults.of(field to first, field to second)
+        val ctx = MockResolverExecutionContext<Query>(
+            internalContext = MockInternalContext(MockSchema.minimal),
+            rootFieldRefResults = recorder,
+        )
+
+        assertSame(first, ctx.rootFieldRef(field, Arguments.NoArguments))
+        assertSame(second, ctx.rootFieldRef(field, Arguments.NoArguments))
+        Assertions.assertEquals(2, recorder.calls.size)
+    }
+
+    @Test
+    fun `RecordingRootFieldRefResults throws when a field is called more times than stubs were configured`() {
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        val recorder = RecordingRootFieldRefResults.of(field to FooResult())
+        val ctx = MockResolverExecutionContext<Query>(
+            internalContext = MockInternalContext(MockSchema.minimal),
+            rootFieldRefResults = recorder,
+        )
+
+        ctx.rootFieldRef(field, Arguments.NoArguments)
+        val ex = assertThrows<IllegalStateException> {
+            ctx.rootFieldRef(field, Arguments.NoArguments)
+        }
+        Assertions.assertTrue(ex.message!!.contains("foo"))
+        Assertions.assertTrue(ex.message!!.contains("2 times"))
+    }
+
+    @Test
+    fun `MockFieldExecutionContext propagates rootFieldRefResults`() {
+        val field = RootObjectFieldImpl<FooParent, FooResult, Arguments.NoArguments>(
+            "foo",
+            fooParent,
+            fooResult,
+            listOf("foo")
+        )
+        val stub = FooResult()
+        val ctx = MockFieldExecutionContext(
+            objectValue = NullObject,
+            queryValue = NullQuery,
+            arguments = Arguments.NoArguments,
+            requestContext = null,
+            selectionsValue = viaduct.api.select.SelectionSet.NoSelections,
+            internalContext = MockInternalContext(MockSchema.minimal),
+            rootFieldRefResults = fixedResult(stub),
+        )
+
+        assertSame(stub, ctx.rootFieldRef(field, Arguments.NoArguments))
+    }
+
+    private object NullObject : Object
+
+    private object NullQuery : Query
 }
