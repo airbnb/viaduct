345bc7b92eee60feaee2fe48c23ec0cefc5cab84
Nestor Marsollier <nmarsollier@users.noreply.github.com>
2026-06-04 17:55:50 +0000

    chore: remove dead `grtPackageName` configuration from `ViaductApplicationExtension` (AIRBNB)
    Github-Change-Id: 1062328
    GitOrigin-RevId: 2949238c1a7c0c3f7c7dab28bcfa7499c010c7c1

--- gradle-plugins/common/src/main/kotlin/viaduct/gradle/ViaductApplicationExtension.kt
+++ gradle-plugins/common/src/main/kotlin/viaduct/gradle/ViaductApplicationExtension.kt
@@ -1,9 +1,6 @@
 package viaduct.gradle
 
 open class ViaductApplicationExtension(objects: org.gradle.api.model.ObjectFactory) {
-    /** Kotlin package name for generated GRT classes. */
-    val grtPackageName = objects.property(String::class.java).convention("viaduct.api.grts")
-
     /** Kotlin package name prefix for all modules. */
     val modulePackagePrefix = objects.property(String::class.java)
 }
